846fe15c2a209b01b36489ca34cfd146c1bddeca
David Green <greensopinion@gmail.com>
2022-01-15 08:58:54 -0800

    add parameters to the example app
    Add UI parameters for resizing the tile
    so that it is easy to experiment with
    different settings.

--- example/lib/tile.dart
+++ example/lib/tile.dart
@@ -1,8 +1,50 @@
+import 'dart:math';
+
 import 'package:example/tile_painter.dart';
 import 'package:flutter/material.dart';
 import 'package:vector_tile_renderer/vector_tile_renderer.dart';
+import 'dart:ui' as ui;
+
+enum RenderMode { vector, raster }
+
+class TileOptions {
+  final Size size;
+  final double scale;
+  final double zoom;
+  final double xOffset;
+  final double yOffset;
+  final RenderMode renderMode;
+
+  TileOptions(
+      {required this.size,
+      required this.scale,
+      required this.zoom,
+      required this.xOffset,
+      required this.yOffset,
+      required this.renderMode});
+
+  TileOptions withValues(
+      {double? scale,
+      Size? size,
+      double? zoom,
+      double? xOffset,
+      double? yOffset,
+      RenderMode? renderMode}) {
+    return TileOptions(
+        size: size ?? this.size,
+        scale: scale ?? this.scale,
+        zoom: zoom ?? this.zoom,
+        xOffset: xOffset ?? this.xOffset,
+        yOffset: yOffset ?? this.yOffset,
+        renderMode: renderMode ?? this.renderMode);
+  }
+}
 
 class Tile extends StatefulWidget {
+  final TileOptions options;
+
+  const Tile({Key? key, required this.options}) : super(key: key);
+
   @override
   State<StatefulWidget> createState() {
     return _TileState();
@@ -12,6 +54,8 @@ class Tile extends StatefulWidget {
 class _TileState extends State<Tile> {
   Tileset? tileset;
   final theme = ProvidedThemes.lightTheme(logger: Logger.console());
+  ui.Image? image;
+  bool _disposed = false;
 
   @override
   void initState() {
@@ -20,15 +64,32 @@ class _TileState extends State<Tile> {
   }
 
   @override
+  void dispose() {
+    super.dispose();
+    _disposed = true;
+    image?.dispose();
+  }
+
+  @override
+  void didUpdateWidget(Tile tile) {
+    super.didUpdateWidget(tile);
+    image?.dispose();
+    image = null;
+    _maybeLoadImage();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    if (tileset == null) {
+    if (tileset == null ||
+        (widget.options.renderMode == RenderMode.raster && image == null)) {
       return CircularProgressIndicator();
     }
     return Container(
-        decoration: BoxDecoration(color: Colors.black45),
+        decoration: BoxDecoration(color: Colors.black45, border: Border.all()),
         child: CustomPaint(
-          size: Size(512, 512),
-          painter: TilePainter(tileset!, theme, scale: 2),
+          size: widget.options.size,
+          painter: TilePainter(tileset!, theme,
+              options: widget.options, image: image),
         ));
   }
 
@@ -43,5 +104,23 @@ class _TileState extends State<Tile> {
     setState(() {
       this.tileset = tileset;
     });
+    _maybeLoadImage();
+  }
+
+  void _maybeLoadImage() async {
+    if (widget.options.renderMode == RenderMode.raster &&
+        image == null &&
+        tileset != null) {
+      final image = await ImageRenderer(theme: theme, scale: 2).render(tileset!,
+          zoom: widget.options.zoom,
+          zoomScaleFactor: pow(2, widget.options.scale).toDouble());
+      if (_disposed) {
+        image.dispose();
+      } else {
+        setState(() {
+          this.image = image;
+        });
+      }
+    }
   }
 }
