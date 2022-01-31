846f1abe14f3f7623610c8c8ca3e9440f9c62b70
David Green <greensopinion@gmail.com>
2022-01-30 16:00:43 -0800

    add theme copy with selected options
    Add Theme.copyWith which enables copying
    a theme with specific layer types.

--- lib/src/themes/theme.dart
+++ lib/src/themes/theme.dart
@@ -9,13 +9,22 @@ class Theme {
   final List<ThemeLayer> layers;
   Theme({required this.id, required this.version, required this.layers});
 
-  Theme atZoom(double zoom) {
+  /// Provides a copy of this theme that only has layers that match
+  /// the given [zoom].
+  Theme atZoom(double zoom) => copyWith(atZoom: zoom);
+
+  /// Creates a copy of this theme with the specified properties.
+  /// If specified, the returned theme only includes layers of the given [types].
+  /// If specified, the returned theme has the given [id].
+  /// If specified, the returned theme has only layers matching the given [atZoom].
+  Theme copyWith({Set<ThemeLayerType>? types, String? id, double? atZoom}) {
     return Theme(
-        id: this.id,
+        id: id ?? this.id,
         version: this.version,
         layers: this
             .layers
-            .where((layer) => _matchesZoom(zoom, layer))
+            .where((layer) => types?.contains(layer.type) ?? true)
+            .where((layer) => atZoom == null || _matchesZoom(atZoom, layer))
             .toList(growable: false));
   }
 
@@ -23,8 +32,11 @@ class Theme {
       (zoom >= (layer.minzoom ?? -1)) && (zoom <= (layer.maxzoom ?? 100));
 }
 
+/// The type of theme layer
 enum ThemeLayerType { fill, line, symbol, background, unsupported }
 
+/// Represents a layer in the theme. Can [render] to a [Context], and specifies
+/// its [type].
 abstract class ThemeLayer {
   final String id;
   final ThemeLayerType type;
