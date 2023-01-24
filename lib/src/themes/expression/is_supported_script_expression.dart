cb2cf7aed29787fcd31d8e499bfbbb91865160ce
Ian Wagner <ian.wagner@stadiamaps.com>
2023-01-24 14:30:56 +0900

    Beef up comment

--- lib/src/themes/expression/is_supported_script_expression.dart
+++ lib/src/themes/expression/is_supported_script_expression.dart
@@ -12,9 +12,19 @@ class IsSupportedScriptExpression extends Expression {
   evaluate(EvaluationContext context) {
     final operand = _expression.evaluate(context);
     if (operand is String) {
-      // At the moment, known rendering frontends utilizing this library make use of advanced
-      // text rendering stacks rather than SDF (per the historical approach of
-      // Mapbox and MapLibre), so complex shaping support is not an issue.
+      // Most MapLibre/Mapbox renderers use SDF glyph rendering, which has
+      // problems with scripts requiring complex text shaping (ex: Khmer,
+      // Nepalese, Burmese, Devanagari, etc.) and, in some cases, lack RTL support.
+      //
+      // At the moment, known rendering frontends utilizing this library, such
+      // as the flutter_map plugin, utilize more traditional font rendering
+      // approaches, so we can assume that all characters are supported.
+      //
+      // More about complex scripts: https://en.wikipedia.org/wiki/Complex_text_layout
+      //
+      // Canonical implementations for SDF-based renderers:
+      // JS: https://github.com/maplibre/maplibre-gl-js/blob/51054671229c68d798fa06a64968aa35688f6a0f/src/util/script_detection.ts#L319
+      // C++: https://github.com/maplibre/maplibre-gl-native/blob/32ed70c95d734590b3e68cd4595a2806fd13c389/src/mbgl/util/i18n.cpp#L632
       return true;
     }
     context.logger.warn(() => 'IsSupportedScriptExpression expected string but got $operand');
