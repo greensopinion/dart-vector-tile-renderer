9a78058b4d77c6fea40f0a2e704662960a824b54
James Green <james@epicrideweather.com>
2025-06-24 11:50:43 -0700

    define line geometry, bind uniform

---
+++ lib/src/gpu/line_geometry.dart
@@ -0,0 +1,62 @@
+
+import 'dart:math';
+import 'dart:typed_data';
+
+import 'package:collection/collection.dart';
+import 'package:flutter_gpu/gpu.dart';
+import 'package:flutter_gpu/gpu.dart' as gpu;
+import 'package:flutter_scene/scene.dart';
+import 'package:vector_math/vector_math.dart';
+import 'package:vector_tile_renderer/src/gpu/shaders.dart';
+
+class LineGeometry extends UnskinnedGeometry {
+  final Iterable<Point<double>> points;
+
+  LineGeometry(this.points) {
+    setVertexShader(shaderLibrary["LineVertex"]!);
+
+    final pointCount = points.length;
+    if (pointCount > 1) {
+      List<double> vertices = List.empty(growable: true);
+      List<int> indices = List.empty(growable: true);
+
+      final segmentCount = pointCount - 1;
+
+      for (int i = 0; i < segmentCount; i++) {
+        double p0 = i + 0;
+        double p1 = i + 1;
+
+        vertices.addAll([p1, 1, p0]);
+        vertices.addAll([p0, 1, p1]);
+        vertices.addAll([p0, 0, p1]);
+        vertices.addAll([p1, 0, p0]);
+
+        indices.addAll([
+          0, 1, 2, 2, 3, 0
+        ].map((it) => it + (4 * i)));
+      }
+
+      uploadVertexData(
+        ByteData.sublistView(Float32List.fromList(vertices)),
+        8,
+        ByteData.sublistView(Uint16List.fromList(indices)),
+        indexType: gpu.IndexType.int16,
+      );
+    }
+  }
+
+
+  @override
+  void bind(RenderPass pass, HostBuffer transientsBuffer, Matrix4 modelTransform, Matrix4 cameraTransform, Vector3 cameraPosition) {
+    super.bind(pass, transientsBuffer, modelTransform, cameraTransform, cameraPosition);
+
+    final linePositions = Float32List.fromList(points.map((it) => [it.x, it.y]).flattened.toList());
+    final linePositionsSlot = vertexShader.getUniformSlot('LinePositions');
+
+    final linePositionsView = transientsBuffer.emplace(
+      linePositions.buffer.asByteData(),
+    );
+
+    pass.bindUniform(linePositionsSlot, linePositionsView);
+  }
+}
--- lib/src/gpu/scene_line_builder.dart
+++ lib/src/gpu/scene_line_builder.dart
@@ -1,7 +1,9 @@
 import 'dart:typed_data';
 
+import 'package:collection/collection.dart';
 import 'package:flutter_gpu/gpu.dart' as gpu;
 import 'package:flutter_scene/scene.dart';
+import 'package:vector_tile_renderer/src/gpu/line_geometry.dart';
 import 'package:vector_tile_renderer/src/gpu/shaders.dart';
 import 'package:vector_tile_renderer/src/themes/feature_resolver.dart';
 import 'package:vector_tile_renderer/src/themes/style.dart';
@@ -20,38 +22,8 @@ class SceneLineBuilder {
   }
 
   void addLine(Style style, LayerFeature feature) {
-    UnskinnedGeometry geometry = UnskinnedGeometry();
-    geometry.setVertexShader(shaderLibrary["LineVertex"]!);
-    final pointCount = feature.feature.modelLines.expand((it) => {it.points}).length;
+    final linePoints = feature.feature.modelLines.expand((it) => {it.points}).flattened;
 
-    List<double> vertices = List.empty(growable: true);
-    List<int> indices = List.empty(growable: true);
-
-    if (pointCount < 2) return;
-
-    final segmentCount = pointCount - 1;
-
-    for (int i = 0; i < segmentCount; i++) {
-      double p0 = i + 0;
-      double p1 = i + 1;
-
-      vertices.addAll([p1, 1, p0]);
-      vertices.addAll([p0, 1, p1]);
-      vertices.addAll([p0, 0, p1]);
-      vertices.addAll([p1, 0, p0]);
-
-      indices.addAll([
-        0, 1, 2, 2, 3, 0
-      ].map((it) => it + (4 * i)));
-    }
-
-    geometry.uploadVertexData(
-      ByteData.sublistView(Float32List.fromList(vertices)),
-      8,
-      ByteData.sublistView(Uint16List.fromList(indices)),
-      indexType: gpu.IndexType.int16,
-    );
-
-    scene.addMesh(Mesh(geometry, UnlitMaterial()));
+    scene.addMesh(Mesh(LineGeometry(linePoints), UnlitMaterial()));
   }
 }
