b7adfce6a1dba106794722be6c7ed55ce17a0d86
James Green <james@epicrideweather.com>
2025-06-25 09:35:03 -0700

    use actual stroke width

--- lib/src/gpu/scene_line_builder.dart
+++ lib/src/gpu/scene_line_builder.dart
@@ -2,9 +2,11 @@ import 'package:collection/collection.dart';
 import 'package:flutter_scene/scene.dart';
 import 'package:vector_tile_renderer/src/gpu/line_geometry.dart';
 import 'package:vector_tile_renderer/src/gpu/line_material.dart';
+import 'package:vector_tile_renderer/src/themes/expression/expression.dart';
 import 'package:vector_tile_renderer/src/themes/feature_resolver.dart';
 import 'package:vector_tile_renderer/src/themes/style.dart';
-import 'package:vector_tile_renderer/src/themes/theme.dart';
+
+import '../../vector_tile_renderer.dart';
 
 class SceneLineBuilder {
   final Scene scene;
@@ -19,10 +21,15 @@ class SceneLineBuilder {
   }
 
   void addLine(Style style, LayerFeature feature) {
-    const double lineWidth = 20;
+
+    EvaluationContext evaluationContext = EvaluationContext(
+            () => {}, TileFeatureType.none, context.logger,
+        zoom: context.zoom, zoomScaleFactor: 1.0, hasImage: (_) => false);
+
+    final double lineWidth = style.linePaint?.evaluate(evaluationContext)?.strokeWidth ?? 0;
     final linePoints = feature.feature.modelLines.expand((it) => {it.points}).flattened;
 
-    if (linePoints.isNotEmpty) {
+    if (linePoints.isNotEmpty && lineWidth > 0) {
       scene.addMesh(Mesh(LineGeometry(linePoints, lineWidth), LineMaterial()));
     }
   }
