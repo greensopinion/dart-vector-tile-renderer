39060526cfc6f73956120561cc79f5af6c0d05a2
James Green <james@epicrideweather.com>
2025-06-26 15:42:40 -0700

    refactor polygon rendering

--- lib/src/gpu/polygon/scene_polygon_builder.dart
+++ lib/src/gpu/polygon/scene_polygon_builder.dart
@@ -1,17 +1,14 @@
 import 'dart:typed_data';
 
-import 'package:flutter_gpu/gpu.dart' as gpu;
 import 'package:flutter_scene/scene.dart';
-import 'package:vector_math/vector_math.dart';
 import 'package:vector_tile_renderer/src/gpu/color_extension.dart';
 import 'package:vector_tile_renderer/src/gpu/colored_material.dart';
-import 'package:vector_tile_renderer/src/gpu/shaders.dart';
+import 'package:vector_tile_renderer/src/gpu/polygon/polygon_geometry.dart';
 import 'package:vector_tile_renderer/src/themes/expression/expression.dart';
 import 'package:vector_tile_renderer/src/themes/feature_resolver.dart';
 import 'package:vector_tile_renderer/src/themes/style.dart';
 
 import '../../../vector_tile_renderer.dart';
-import 'earcut_polygons.dart';
 
 class ScenePolygonBuilder {
   final Scene scene;
@@ -26,39 +23,20 @@ class ScenePolygonBuilder {
   }
 
   void addPolygon(Style style, LayerFeature feature) {
-
     final polygons = feature.feature.modelPolygons;
 
     for (final polygon in polygons) {
-
       EvaluationContext evaluationContext = EvaluationContext(
-              () => {}, TileFeatureType.none, context.logger,
+          () => {}, TileFeatureType.none, context.logger,
           zoom: context.zoom, zoomScaleFactor: 1.0, hasImage: (_) => false);
 
-      final fillPaint = style.fillPaint?.evaluate(evaluationContext)?.color.vector4;
-
+      final fillPaint =
+          style.fillPaint?.evaluate(evaluationContext)?.color.vector4;
       if (fillPaint == null) {
         return;
       }
 
-      final normalized = <double>[];
-      final fixedIndices = <int>[];
-
-      triangulatePolygonToBuffers(polygon, normalized, fixedIndices);
-
-      scene.addMesh(
-        Mesh(
-          UnskinnedGeometry()
-              ..setVertexShader(shaderLibrary["SimpleVertex"]!)
-              ..uploadVertexData(
-                ByteData.sublistView(Float32List.fromList(normalized)),
-                  normalized.length ~/ 3,
-                ByteData.sublistView(Uint16List.fromList(fixedIndices)),
-                indexType: gpu.IndexType.int16,
-              ),
-            ColoredMaterial(fillPaint)
-        )
-      );
+      scene.addMesh(Mesh(PolygonGeometry(polygon), ColoredMaterial(fillPaint)));
     }
   }
 }
