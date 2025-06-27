44c995ea681580c0f6e900c7d6d4b6e1a2297b30
James Green <james@epicrideweather.com>
2025-06-27 12:29:25 -0700

    scale canvas and rendering to draw tiles at device screen resolution
    Note: This may cause visual issues if the canvas is not already clipped
    to the correct size. I have added tile clipping to /example but
    not the main library.

--- lib/src/gpu/tile_renderer.dart
+++ lib/src/gpu/tile_renderer.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:ui' as ui;
 import 'dart:math' as math;
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_scene/scene.dart';
 import 'package:vector_math/vector_math.dart' as vm;
 import 'package:vector_tile_renderer/src/gpu/scene_building_visitor.dart';
@@ -15,6 +16,9 @@ import '../tileset.dart';
 /// this class is stateful, designed to be reused for rendering a tile
 /// multiple times.
 ///
+
+const int maxTextureSize = 16384;
+
 class TileRenderer {
   static final Completer<void> _initializer = Completer<void>();
   static Future<void> initialize = _initializer.future;
@@ -65,7 +69,28 @@ class TileRenderer {
   }
 
   void render(ui.Canvas canvas, ui.Size size) {
-    scene.render(_camera, canvas, viewport: ui.Offset.zero & size);
+    ui.Size canvasScale = getCanvasScale(canvas);
+    canvas.scale(canvasScale.width, canvasScale.height);
+
+    scene.render(_camera, canvas, viewport: ui.Offset.zero & canvas.getLocalClipBounds().size);
+  }
+
+  ui.Size getCanvasScale(ui.Canvas canvas) {
+    ui.Size src = canvas.getLocalClipBounds().size;
+    ui.Size dest = canvas.getDestinationClipBounds().size;
+
+    final view = ui.PlatformDispatcher.instance.views.first;
+    final pixelRatio = view.display.devicePixelRatio;
+
+    final resultSize = ui.Size(dest.width * pixelRatio, dest.height * pixelRatio);
+    final result = ui.Size(src.width / resultSize.width, src.height / resultSize.height);
+
+    if (result.isFinite && resultSize.isFinite && resultSize.longestSide < maxTextureSize) {
+      return result;
+    } else {
+      // scale to max size -1 for floating point safety
+      return ui.Size(src.width / (maxTextureSize - 1), src.height / (maxTextureSize - 1));
+    }
   }
 
   Scene _createScene() {
