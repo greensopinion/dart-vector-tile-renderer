c9fdf862eef06bd057d13beb0f0cdce998430f4f
James Green <james@epicrideweather.com>
2025-07-16 15:45:51 -0700

    respect scope param for raster tiles

---
+++ lib/src/gpu/raster/raster_geometry.dart
@@ -0,0 +1,34 @@
+import 'dart:typed_data';
+import 'package:flutter_gpu/gpu.dart' as gpu;
+import 'package:flutter_scene/scene.dart';
+import 'package:vector_tile_renderer/src/gpu/shaders.dart';
+import 'package:vector_tile_renderer/vector_tile_renderer.dart';
+
+class RasterGeometry extends UnskinnedGeometry {
+
+  RasterGeometry(RasterTile tile) {
+    setVertexShader(shaderLibrary["RasterVertex"]!);
+
+    final texture = tile.texture;
+    if (texture != null && texture.isValid) {
+      final top = tile.scope.top / texture.height;
+      final bottom = tile.scope.bottom / texture.height;
+      final left = tile.scope.left / texture.width;
+      final right = tile.scope.right / texture.width;
+
+      List<double> vertices = [
+        -1, -1, 0, left, bottom,
+        1, -1, 0, right, bottom,
+        1, 1, 0, right, top,
+        -1, 1, 0, left, top,
+      ];
+
+      uploadVertexData(
+        ByteData.sublistView(Float32List.fromList(vertices)),
+        4,
+        ByteData.sublistView(Uint16List.fromList([0, 1, 2, 2, 3, 0])),
+        indexType: gpu.IndexType.int16,
+      );
+    }
+  }
+}
---
+++ shaders/raster.vert
@@ -0,0 +1,26 @@
+uniform FrameInfo {
+    mat4 model_transform;
+    mat4 camera_transform;
+    vec3 camera_position;
+}
+frame_info;
+
+in vec3 position;
+in vec2 uv;
+
+out vec3 v_position;
+out vec3 v_normal;
+out vec3 v_viewvector;
+out vec2 v_texture_coords;
+out vec4 v_color;
+
+void main() {
+
+    gl_Position = vec4(position, 1.0);
+
+    v_position = position;
+    v_viewvector = frame_info.camera_position - v_position;
+    v_normal = vec3(1,0,0);
+    v_texture_coords = uv;
+    v_color = vec4(1,1,1,1);
+}
