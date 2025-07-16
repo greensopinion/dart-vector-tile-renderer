114a1861de59f27810ecc7eb2bf67f743b63239b
James Green <james@epicrideweather.com>
2025-07-16 10:24:35 -0700

    load multiple tile sources, add contour lines to example

--- example/lib/tile.dart
+++ example/lib/tile.dart
@@ -3,6 +3,8 @@ import 'dart:math';
 import 'package:example/tile_painter.dart';
 import 'package:flutter/material.dart';
 import 'package:vector_tile_renderer/vector_tile_renderer.dart';
+// import again due to shadowed class name
+import 'package:vector_tile_renderer/src/model/tile_model.dart' as tile_model;
 import 'dart:ui' as ui;
 
 enum RenderMode { vector, raster }
@@ -105,10 +107,7 @@ class _TileState extends State<Tile> {
     return Container(
         decoration: BoxDecoration(color: Colors.black45, border: Border.all()),
         child: isRaster
-            ? RawImage(
-                image: image!,
-                width: widget.options.size.width,
-                height: widget.options.size.height)
+            ? RawImage(image: image!, width: widget.options.size.width, height: widget.options.size.height)
             : CustomPaint(
                 size: widget.options.size,
                 painter: TilePainter(tileset!, theme, options: widget.options),
@@ -116,41 +115,37 @@ class _TileState extends State<Tile> {
   }
 
   void _loadTileset() async {
-    final tileBuffer =
-        await DefaultAssetBundle.of(context).load('assets/sample_tile.pbf');
-    final tileBytes = tileBuffer.buffer
-        .asUint8List(tileBuffer.offsetInBytes, tileBuffer.lengthInBytes);
-    var tileData = TileFactory(theme, const Logger.noop())
-        .createTileData(VectorTileReader().read(tileBytes));
-    if (widget.options.clipSize > 0) {
-      final clipSize = widget.options.clipSize;
-      final clipOffsetX = widget.options.clipOffsetX;
-      final clipOffsetY = widget.options.clipOffsetY;
-      final clip = TileClip(
-          bounds: Rectangle(clipOffsetX, clipOffsetY, clipSize, clipSize));
-      tileData = clip.clip(tileData);
-      if (clipOffsetX > 0 || clipOffsetY > 0) {
-        tileData = TileTranslate(Point(-clipOffsetX, -clipOffsetY))
-            .translate(tileData);
-      }
-    }
-    final tile = tileData.toTile();
-    final tileset = TilesetPreprocessor(theme)
-        .preprocess(Tileset({'openmaptiles': tile}), zoom: 14);
+    tile_model.Tile tile = await loadTile('assets/11_325_699_openmaptiles.pbf');
+    tile_model.Tile contour = await loadTile('assets/11_325_699_contour.pbf');
+    final tileset =
+        TilesetPreprocessor(theme).preprocess(Tileset({'openmaptiles': tile, 'contour': contour}), zoom: 14);
     setState(() {
       this.tileset = tileset;
     });
     _maybeLoadImage();
   }
 
+  Future<tile_model.Tile> loadTile(String path) async {
+    final tileBuffer = await DefaultAssetBundle.of(context).load(path);
+    final tileBytes = tileBuffer.buffer.asUint8List(tileBuffer.offsetInBytes, tileBuffer.lengthInBytes);
+    var tileData = TileFactory(theme, const Logger.noop()).createTileData(VectorTileReader().read(tileBytes));
+    if (widget.options.clipSize > 0) {
+      final clipSize = widget.options.clipSize;
+      final clipOffsetX = widget.options.clipOffsetX;
+      final clipOffsetY = widget.options.clipOffsetY;
+      final clip = TileClip(bounds: Rectangle(clipOffsetX, clipOffsetY, clipSize, clipSize));
+      tileData = clip.clip(tileData);
+      if (clipOffsetX > 0 || clipOffsetY > 0) {
+        tileData = TileTranslate(Point(-clipOffsetX, -clipOffsetY)).translate(tileData);
+      }
+    }
+    return tileData.toTile();
+  }
+
   void _maybeLoadImage() async {
-    if (widget.options.renderMode == RenderMode.raster &&
-        image == null &&
-        tileset != null) {
-      final image = await ImageRenderer(theme: theme, scale: 2).render(
-          TileSource(tileset: tileset!),
-          zoom: widget.options.zoom,
-          zoomScaleFactor: pow(2, widget.options.scale).toDouble());
+    if (widget.options.renderMode == RenderMode.raster && image == null && tileset != null) {
+      final image = await ImageRenderer(theme: theme, scale: 2).render(TileSource(tileset: tileset!),
+          zoom: widget.options.zoom, zoomScaleFactor: pow(2, widget.options.scale).toDouble());
       if (_disposed) {
         image.dispose();
       } else {
