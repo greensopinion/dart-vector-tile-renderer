d69f11a62a50bc9f99282b5d0890fda65bae1a02
James Green <james@epicrideweather.com>
2025-07-30 12:35:09 -0700

    optimize background geom

--- lib/src/gpu/background/background_geometry.dart
+++ lib/src/gpu/background/background_geometry.dart
@@ -1,26 +1,25 @@
 
 import 'dart:typed_data';
-import 'package:flutter_gpu/gpu.dart' as gpu;
 import 'package:flutter_scene/scene.dart';
 import 'package:vector_tile_renderer/src/gpu/shaders.dart';
 
 class BackgroundGeometry extends UnskinnedGeometry {
 
+  static final _vertices = ByteData.sublistView(Float32List.fromList([
+    -1, -1,  0,
+     1, -1,  0,
+     1,  1,  0,
+    -1,  1,  0,
+  ]));
+
+  static final _indices = ByteData.sublistView(Uint16List.fromList([
+    0, 1, 2,
+    2, 3, 0,
+  ]));
+
   BackgroundGeometry() {
     setVertexShader(shaderLibrary["SimpleVertex"]!);
 
-    List<double> vertices = [
-      -1, -1,  0,
-       1, -1,  0,
-       1,  1,  0,
-      -1,  1,  0
-    ];
-
-    uploadVertexData(
-      ByteData.sublistView(Float32List.fromList(vertices)),
-      4,
-      ByteData.sublistView(Uint16List.fromList([0,1,2,2,3,0])),
-      indexType: gpu.IndexType.int16,
-    );
+    uploadVertexData(_vertices, 4, _indices);
   }
 }
