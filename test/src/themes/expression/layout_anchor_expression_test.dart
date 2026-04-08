5f3b21197493e3c376dc70da69a8789a9ae5cb5e
David Green <greensopinion@gmail.com>
2026-04-07 19:54:05 -0700

    formatting

--- test/src/themes/expression/layout_anchor_expression_test.dart
+++ test/src/themes/expression/layout_anchor_expression_test.dart
@@ -7,42 +7,37 @@ import 'package:vector_tile_renderer/vector_tile_renderer.dart';
 
 void main() {
   EvaluationContext context() => EvaluationContext(
-        () => {},
-        TileFeatureType.point,
-        const Logger.noop(),
-        zoom: 1.0,
-        zoomScaleFactor: 1.0,
-        hasImage: (_) => false,
-      );
+    () => {},
+    TileFeatureType.point,
+    const Logger.noop(),
+    zoom: 1.0,
+    zoomScaleFactor: 1.0,
+    hasImage: (_) => false,
+  );
 
   group('LayoutAnchorExpression', () {
     test('evaluates bottom string to LayoutAnchor.bottom', () {
-      final expression =
-          LayoutAnchorExpression(LiteralExpression('bottom'));
+      final expression = LayoutAnchorExpression(LiteralExpression('bottom'));
       expect(expression.evaluate(context()), equals(LayoutAnchor.bottom));
     });
 
     test('evaluates top string to LayoutAnchor.top', () {
-      final expression =
-          LayoutAnchorExpression(LiteralExpression('top'));
+      final expression = LayoutAnchorExpression(LiteralExpression('top'));
       expect(expression.evaluate(context()), equals(LayoutAnchor.top));
     });
 
     test('evaluates center string to LayoutAnchor.center', () {
-      final expression =
-          LayoutAnchorExpression(LiteralExpression('center'));
+      final expression = LayoutAnchorExpression(LiteralExpression('center'));
       expect(expression.evaluate(context()), equals(LayoutAnchor.center));
     });
 
     test('evaluates null to DEFAULT', () {
-      final expression =
-          LayoutAnchorExpression(LiteralExpression(null));
+      final expression = LayoutAnchorExpression(LiteralExpression(null));
       expect(expression.evaluate(context()), equals(LayoutAnchor.DEFAULT));
     });
 
     test('evaluates unknown string to DEFAULT', () {
-      final expression =
-          LayoutAnchorExpression(LiteralExpression('left'));
+      final expression = LayoutAnchorExpression(LiteralExpression('left'));
       expect(expression.evaluate(context()), equals(LayoutAnchor.DEFAULT));
     });
   });
